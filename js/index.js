d261aa64f7670b73cc25ffbdbc13bdee75c3b5b4
retr0hax <121464454+TrueRetr0@users.noreply.github.com>
2023-07-17 18:52:57 -0700

    fix

--- js/index.js
+++ js/index.js
@@ -2,5 +2,9 @@ const revealButton = document.querySelector('#reveal-button');
 const textContainer = document.querySelector('#text-container');
 
 revealButton.addEventListener('click', () => {
-  textContainer.style.display = 'block';
+  revealButton.style.opacity = 0;
+  setTimeout(() => {
+    revealButton.style.display = 'none';
+    textContainer.style.display = 'block';
+  }, 1000);
 });
