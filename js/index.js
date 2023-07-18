4010722503a65b803cf3734ac7b967c39144e5b0
retr0hax <121464454+TrueRetr0@users.noreply.github.com>
2023-07-17 19:00:32 -0700

    test

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ const revealButton = document.querySelector('#reveal-button');
 const textContainer = document.querySelector('#text-container');
 
 revealButton.addEventListener('click', () => {
-  revealButton.style.opacity = 0;
-  setTimeout(() => {
-    revealButton.style.display = 'none';
-    textContainer.style.display = 'block';
-  }, 1000);
+    revealButton.style.opacity = '0';
+    setTimeout(() => {
+        revealButton.style.display = 'none';
+        textContainer.style.display = 'block';
+    },1000);
 });
