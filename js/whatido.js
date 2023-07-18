dbd8b0f707e47cf4aedc226873aac37119312ea6
retr0hax <121464454+TrueRetr0@users.noreply.github.com>
2023-07-18 13:25:42 -0700

    update other pages as well

---
+++ js/whatido.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('click', function(event) {
+      const x = event.clientX;
+      const y = event.clientY;
+  
+      const colors = ['red', 'white', 'blue'];
+      colors.forEach(color => createFirework(x, y, color));
+    });
+  });
+  
+  function createFirework(x, y, color) {
+    const firework = document.createElement('div');
+    firework.classList.add('firework', color);
+    firework.style.left = `${x}px`;
+    firework.style.top = `${y}px`;
+  
+    document.body.appendChild(firework);
+  
+    // Remove the firework element after the animation ends
+    firework.addEventListener('animationend', function() {
+      firework.remove();
+    });
+  }
+  
