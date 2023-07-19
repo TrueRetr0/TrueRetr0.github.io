4e0b4dc0b4062c6685744801ed6924aeef082b01
retr0hax <121464454+TrueRetr0@users.noreply.github.com>
2023-07-18 20:55:38 -0700

    secret konami code update

--- js/index.js
+++ js/index.js
@@ -21,4 +21,56 @@ document.addEventListener('DOMContentLoaded', function() {
       firework.remove();
     });
   }
+
+const konamiCodeSequence = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+let konamiCodeIndex = 0;
+let stickmanVisible = false;
+  
+document.addEventListener('keydown', function(event) {
+  const key = event.key.toLowerCase();
+  
+    if (key === konamiCodeSequence[konamiCodeIndex]) {
+      konamiCodeIndex++;
+      if (konamiCodeIndex === konamiCodeSequence.length) {
+        revealStickman();
+        konamiCodeIndex = 0;
+      }
+    } else {
+      konamiCodeIndex = 0;
+    }
+  
+    if (stickmanVisible) {
+      handleStickmanMovement(key);
+    }
+  });
+  
+  function revealStickman() {
+    const stickman = document.getElementById('stickman');
+    stickmanVisible = true;
+    stickman.style.top = '50%';
+    stickman.style.left = '50%';
+  }
+  
+  function handleStickmanMovement(key) {
+    const stickman = document.getElementById('stickman');
+    const stepSize = 10;
+  
+    switch (key) {
+      case 'arrowup':
+        stickman.style.top = parseInt(stickman.style.top) - stepSize + 'px';
+        break;
+      case 'arrowdown':
+        stickman.style.top = parseInt(stickman.style.top) + stepSize + 'px';
+        break;
+      case 'arrowleft':
+        stickman.style.left = parseInt(stickman.style.left) - stepSize + 'px';
+        break;
+      case 'arrowright':
+        stickman.style.left = parseInt(stickman.style.left) + stepSize + 'px';
+        break;
+      default:
+        break;
+    }
+  }
+  
   
