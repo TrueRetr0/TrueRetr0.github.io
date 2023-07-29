66a1752fc92b81b209fe107b5d8c270919a1c899
retr0hax <121464454+TrueRetr0@users.noreply.github.com>
2023-07-29 11:29:23 -0700

    big fixes

--- js/index.js
+++ js/index.js
@@ -1,28 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.body.addEventListener('click', function(event) {
-      const x = event.clientX;
-      const y = event.clientY;
-  
-      const colors = ['red', 'white', 'blue'];
-      colors.forEach(color => createFirework(x, y, color));
-    });
-  });
-  
-  function createFirework(x, y, color) {
-    const firework = document.createElement('div');
-    firework.classList.add('firework', color);
-    firework.style.left = `${x}px`;
-    firework.style.top = `${y}px`;
-  
-    document.body.appendChild(firework);
-  
-    // Remove the firework element after the animation ends
-    firework.addEventListener('animationend', function() {
-      firework.remove();
-    });
-  }
+  document.body.addEventListener('click', function(event) {
+    const x = event.clientX;
+    const y = event.clientY;
 
-  // Function to toggle the visibility of the site settings menu
+    const colors = ['red', 'white', 'blue'];
+    colors.forEach(color => createFirework(x, y, color));
+  });
+});
+
+function createFirework(x, y, color) {
+  const firework = document.createElement('div');
+  firework.classList.add('firework', color);
+  firework.style.left = `${x}px`;
+  firework.style.top = `${y}px`;
+
+  document.body.appendChild(firework);
+
+  firework.addEventListener('animationend', function() {
+    firework.remove();
+  });
+}
+
+// Function to toggle the visibility of the site settings menu
 function toggleSettings() {
   const settingsMenu = document.getElementById('settings-menu');
   settingsMenu.classList.toggle('settings-hidden');
@@ -40,5 +39,3 @@ function changeFontSize() {
   const fontSize = fontSizeInput.value + 'px';
   document.body.style.fontSize = fontSize;
 }
-
-  
